c65999a7ab4a2c3d61d699aeb4922476a6f7f612
yoon82@SLAC.STANFORD.EDU <yoon82@SLAC.STANFORD.EDU@b967ad99-d558-0410-b138-e0f6c56caec7>
2016-12-16 06:10:11 +0000

    Tagging psocake with V00-01-51

---
+++ psocake/tags/V00-01-51/src/LaunchIndexer.py
@@ -0,0 +1,126 @@
+from pyqtgraph.Qt import QtCore
+import subprocess
+import os, shlex
+import numpy as np
+
+class LaunchIndexer(QtCore.QThread):
+    def __init__(self, parent = None):
+        QtCore.QThread.__init__(self, parent)
+        self.parent = parent
+        self.experimentName = None
+        self.detInfo = None
+
+    def __del__(self):
+        self.exiting = True
+        self.wait()
+
+    def launch(self,experimentName,detInfo): # Pass in peak parameters
+        self.experimentName = experimentName
+        self.detInfo = detInfo
+        self.start()
+
+    def digestRunList(self,runList):
+        runsToDo = []
+        if not runList:
+            print "Run(s) is empty. Please type in the run number(s)."
+            return runsToDo
+        runLists = str(runList).split(",")
+        for list in runLists:
+            temp = list.split(":")
+            if len(temp) == 2:
+                for i in np.arange(int(temp[0]),int(temp[1])+1):
+                    runsToDo.append(i)
+            elif len(temp) == 1:
+                runsToDo.append(int(temp[0]))
+        return runsToDo
+
+    def saveCheetahFormatMask(self, run, arg):
+        import h5py
+        if arg == 'lcls':
+            if 'cspad' in self.parent.detInfo.lower() and 'cxi' in self.parent.experimentName:
+                dim0 = 8 * 185
+                dim1 = 4 * 388
+            elif 'rayonix' in self.parent.detInfo.lower() and 'mfx' in self.parent.experimentName:
+                dim0 = 1920
+                dim1 = 1920
+            elif 'rayonix' in self.parent.detInfo.lower() and 'xpp' in self.parent.experimentName:
+                dim0 = 1920
+                dim1 = 1920
+            else:
+                print "saveCheetahFormatMask not implemented"
+
+            fname = self.parent.index.outDir+'/r'+str(run).zfill(4)+'/staticMask.h5'
+            print "Saving static mask in Cheetah format: ", fname
+            myHdf5 = h5py.File(fname, 'w')
+            dset = myHdf5.create_dataset('/entry_1/data_1/mask', (dim0,dim1), dtype='int')
+
+            # Convert calib image to cheetah image
+            if self.parent.mk.combinedMask is None:
+                img = np.ones((dim0, dim1))
+            else:
+                img = np.zeros((dim0, dim1))
+                counter = 0
+                if 'cspad' in self.parent.detInfo.lower() and 'cxi' in self.parent.experimentName:
+                    for quad in range(4):
+                        for seg in range(8):
+                            img[seg * 185:(seg + 1) * 185, quad * 388:(quad + 1) * 388] = self.parent.mk.combinedMask[counter, :, :]
+                            counter += 1
+                elif 'rayonix' in self.parent.detInfo.lower() and 'mfx' in self.parent.experimentName:
+                    img = self.parent.mk.combinedMask[counter, :, :] # psana format
+                elif 'rayonix' in self.parent.detInfo.lower() and 'xpp' in self.parent.experimentName:
+                    img = self.parent.mk.combinedMask[counter, :, :] # psana format
+            dset[:,:] = img
+            myHdf5.close()
+
+    def run(self):
+        # Digest the run list
+        runsToDo = self.digestRunList(self.parent.index.runs)
+
+        for run in runsToDo:
+            runDir = self.parent.index.outDir+"/r"+str(run).zfill(4)
+            try:
+                if os.path.exists(runDir) is False:
+                    os.makedirs(runDir, 0774)
+            except:
+                print "No write access to: ", runDir
+
+            # Generate Cheetah mask
+            #self.saveCheetahFormatMask(run, 'lcls')
+
+            # Update elog
+            try:
+                if self.parent.exp.logger == True:
+                    self.parent.exp.table.setValue(run,"Number of indexed","#IndexingNow")
+            except AttributeError:
+                print "e-Log table does not exist"
+
+            cmd = "indexCrystals" + \
+                  " -e " + self.parent.experimentName + \
+                  " -d " + self.parent.detInfo + \
+                  " --geom " + self.parent.index.geom + \
+                  " --peakMethod " + self.parent.index.peakMethod + \
+                  " --integrationRadius " + self.parent.index.intRadius + \
+                  " --indexingMethod " + self.parent.index.indexingMethod + \
+                  " --minPeaks " + str(self.parent.pk.minPeaks) + \
+                  " --maxPeaks " + str(self.parent.pk.maxPeaks) + \
+                  " --minRes " + str(self.parent.pk.minRes) + \
+                  " --tolerance " + str(self.parent.index.tolerance) + \
+                  " --outDir " + self.parent.index.outDir + \
+                  " --sample " + self.parent.index.sample + \
+                  " --queue " + self.parent.index.queue + \
+                  " --chunkSize " + str(self.parent.index.chunkSize) + \
+                  " --noe " + str(self.parent.index.noe) + \
+                  " --instrument " + self.parent.det.instrument() + \
+                  " --pixelSize " + str(self.parent.pixelSize) + \
+                  " --coffset " + str(self.parent.coffset) + \
+                  " --clenEpics " + self.parent.clenEpics + \
+                  " --logger " + str(self.parent.exp.logger) + \
+                  " --hitParam_threshold " + str(self.parent.pk.hitParam_threshold) + \
+                  " --keepData " + str(self.parent.index.keepData) + \
+                  " -v " + str(self.parent.args.v)
+            if self.parent.index.tag: cmd += " --tag " + self.parent.index.tag
+            if self.parent.index.pdb: cmd += " --pdb " + self.parent.index.pdb
+            if self.parent.index.extra: cmd += " " + self.parent.index.extra
+            cmd += " --run " + str(run)
+            print "Launch indexing job: ", cmd
+            p = subprocess.Popen(shlex.split(cmd))
---
+++ psocake/tags/V00-01-51/src/peakFinderClient.py
@@ -0,0 +1,218 @@
+import psana
+import numpy as np
+from mpidata import mpidata 
+import PeakFinder as pf
+import psanaWhisperer
+import time
+
+from mpi4py import MPI
+comm = MPI.COMM_WORLD
+rank = comm.Get_rank()
+size = comm.Get_size()
+
+def runclient(args):
+    ds = psana.DataSource("exp="+args.exp+":run="+str(args.run)+':idx')
+    run = ds.runs().next()
+    env = ds.env()
+    times = run.times()
+    d = psana.Detector(args.det)
+    d.do_reshape_2d_to_3d(flag=True)
+
+    ps = psanaWhisperer.psanaWhisperer(args.exp, args.run, args.det, args.clen, args.localCalib)
+    ps.setupExperiment()
+
+    ebeamDet = psana.Detector('EBeam')
+
+    hasCoffset = False
+    hasDetectorDistance = False
+    if args.detectorDistance is not 0:
+        hasDetectorDistance = True
+    if args.coffset is not 0:
+        hasCoffset = True
+
+    if hasCoffset:
+        try:
+            detectorDistance = args.coffset + ps.clen * 1e-3  # sample to detector in m
+        except:
+            detectorDistance = 0
+    elif hasDetectorDistance:
+        detectorDistance = args.detectorDistance
+
+    for nevent in np.arange(len(times)):
+        if nevent == args.noe : break
+        if nevent%(size-1) != rank-1: continue # different ranks look at different events
+        evt = run.event(times[nevent])
+        if args.profile:
+            tic = time.time()
+            print "peakFinderClient: ", args.profile
+
+        detarr = d.calib(evt)
+
+        if args.profile:
+            calibTime = time.time() - tic # Time to calibrate per event
+
+        if detarr is None: continue
+
+        # Initialize hit finding
+        if not hasattr(d,'peakFinder'):
+            if args.algorithm == 1:
+                d.peakFinder = pf.PeakFinder(env.experiment(),evt.run(),args.det,evt,d,
+                                          args.algorithm, args.alg_npix_min,
+                                          args.alg_npix_max, args.alg_amax_thr,
+                                          args.alg_atot_thr, args.alg_son_min,
+                                          alg1_thr_low=args.alg1_thr_low,
+                                          alg1_thr_high=args.alg1_thr_high,
+                                          alg1_rank=args.alg1_rank,
+                                          alg1_radius=args.alg1_radius,
+                                          alg1_dr=args.alg1_dr,
+                                          streakMask_on=args.streakMask_on,
+                                          streakMask_sigma=args.streakMask_sigma,
+                                          streakMask_width=args.streakMask_width,
+                                          userMask_path=args.userMask_path,
+                                          psanaMask_on=args.psanaMask_on,
+                                          psanaMask_calib=args.psanaMask_calib,
+                                          psanaMask_status=args.psanaMask_status,
+                                          psanaMask_edges=args.psanaMask_edges,
+                                          psanaMask_central=args.psanaMask_central,
+                                          psanaMask_unbond=args.psanaMask_unbond,
+                                          psanaMask_unbondnrs=args.psanaMask_unbondnrs,
+                                          medianFilterOn=args.medianBackground,
+                                          medianRank=args.medianRank,
+                                          radialFilterOn=args.radialBackground,
+                                          distance=args.detectorDistance,
+                                          minNumPeaks=args.minPeaks,
+                                          maxNumPeaks=args.maxPeaks,
+                                          minResCutoff=args.minRes,
+                                          clen=args.clen,
+                                          localCalib=args.localCalib)
+        if args.profile:
+            tic = time.time()
+
+        d.peakFinder.findPeaks(detarr,evt)
+
+        if args.profile:
+            peakTime = time.time() - tic # Time to find the peaks per event
+        md=mpidata()
+        md.addarray('peaks',d.peakFinder.peaks)
+        md.small.eventNum = nevent
+        md.small.maxRes = d.peakFinder.maxRes
+        md.small.powder = 0
+        if args.profile:
+            md.small.calibTime = calibTime
+            md.small.peakTime = peakTime
+
+        # other cxidb data
+        ps.getEvent(nevent)
+
+        es = ps.ds.env().epicsStore()
+        try:
+            pulseLength = es.value('SIOC:SYS0:ML00:AO820') * 1e-15  # s
+        except:
+            pulseLength = 0
+
+        md.small.pulseLength = pulseLength
+
+        md.small.detectorDistance = detectorDistance
+
+        md.small.pixelSize = args.pixelSize
+
+        # LCLS
+        if "cxi" in args.exp:
+            md.small.lclsDet = es.value(args.clen)  # mm
+        elif "mfx" in args.exp:
+            md.small.lclsDet = es.value(args.clen)  # mm
+        elif "xpp" in args.exp:
+            md.small.lclsDet = es.value(args.clen)  # mm
+
+        try:
+            md.small.ebeamCharge = es.value('BEND:DMP1:400:BDES')
+        except:
+            md.small.ebeamCharge = 0
+
+        try:
+            md.small.beamRepRate = es.value('EVNT:SYS0:1:LCLSBEAMRATE')
+        except:
+            md.small.beamRepRate = 0
+
+        try:
+            md.small.particleN_electrons = es.value('BPMS:DMP1:199:TMIT1H')
+        except:
+            md.small.particleN_electrons = 0
+
+        try:
+            md.small.eVernier = es.value('SIOC:SYS0:ML00:AO289')
+        except:
+            md.small.eVernier = 0
+
+        try:
+            md.small.charge = es.value('BEAM:LCLS:ELEC:Q')
+        except:
+            md.small.charge = 0
+
+        try:
+            md.small.peakCurrentAfterSecondBunchCompressor = es.value('SIOC:SYS0:ML00:AO195')
+        except:
+            md.small.peakCurrentAfterSecondBunchCompressor = 0
+
+        try:
+            md.small.pulseLength = es.value('SIOC:SYS0:ML00:AO820')
+        except:
+            md.small.pulseLength = 0
+
+        try:
+            md.small.ebeamEnergyLossConvertedToPhoton_mJ = es.value('SIOC:SYS0:ML00:AO569')
+        except:
+            md.small.ebeamEnergyLossConvertedToPhoton_mJ = 0
+
+        try:
+            md.small.calculatedNumberOfPhotons = es.value('SIOC:SYS0:ML00:AO580') * 1e12  # number of photons
+        except:
+            md.small.calculatedNumberOfPhotons = 0
+
+        try:
+            md.small.photonBeamEnergy = es.value('SIOC:SYS0:ML00:AO541')
+        except:
+            md.small.photonBeamEnergy = 0
+
+        try:
+            md.small.wavelength = es.value('SIOC:SYS0:ML00:AO192')
+        except:
+            md.small.wavelength = 0
+
+        ebeam = ebeamDet.get(ps.evt)#.get(psana.Bld.BldDataEBeamV7, psana.Source('BldInfo(EBeam)'))
+        try:
+            photonEnergy = ebeam.ebeamPhotonEnergy()
+            pulseEnergy = ebeam.ebeamL3Energy()  # MeV
+        except:
+            photonEnergy = 0
+            pulseEnergy = 0
+            if md.small.wavelength > 0:
+                h = 6.626070e-34  # J.m
+                c = 2.99792458e8  # m/s
+                joulesPerEv = 1.602176621e-19  # J/eV
+                photonEnergy = (h / joulesPerEv * c) / (md.small.wavelength * 1e-9)
+
+        md.small.photonEnergy = photonEnergy
+        md.small.pulseEnergy = pulseEnergy
+
+        evtId = ps.evt.get(psana.EventId)
+        md.small.sec = evtId.time()[0]
+        md.small.nsec = evtId.time()[1]
+        md.small.fid = evtId.fiducials()
+        md.small.evtNum = nevent
+
+        if len(d.peakFinder.peaks) >= args.minPeaks and \
+           len(d.peakFinder.peaks) <= args.maxPeaks and \
+           d.peakFinder.maxRes >= args.minRes:
+            # Write image in cheetah format
+            img = ps.getCheetahImg()
+            #assert (img is not None)
+            if img is not None: md.addarray('data', img)
+        md.send() # send mpi data object to master when desired
+    # At the end of the run, send the powder of hits and misses
+    md = mpidata()
+    md.small.powder = 1
+    md.addarray('powderHits', d.peakFinder.powderHits)
+    md.addarray('powderMisses', d.peakFinder.powderMisses)
+    md.send()
+    md.endrun()
